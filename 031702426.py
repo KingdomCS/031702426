5af9ac6745965688c38642c548d53f831afd959d
hsthy <1821321180@qq.com>
2019-09-29 00:54:13 +0800

    解决了多行输入

--- 031702426.py
+++ 031702426.py
@@ -6,6 +6,7 @@ import os
 import sys
 
 #定义标准答案的格式,名字+手机+七级地址
+
 class Result:
     name = ""
     phone = ""
@@ -16,19 +17,7 @@ class Result:
     street = ""#路名
     number = ""#门牌号
     building = ""#详细地址
-result = Result()
-#用于输出的json
-data={
-    "姓名":"",
-    "手机":"",
-    "地址":[]
-    }
-jsondata = {}
-filepath = os.path.split(os.path.realpath(__file__))[0]
-filepath = filepath + "\\" + "pcas-code.json"
-with open(filepath,"r+",encoding='utf-8_sig')as f:
-    jsondata = json.load(f)#将已编码JSON字符串解码为Python字典
-    
+
 #获取电话
 def getphone(s):
     pattern = re.compile(r'1\d{10}')
@@ -55,61 +44,61 @@ def cutdown(origin,child):
         i = i + 1
     return origin[i:]
     
-def segementFive(address):
+def segementFive(add,res,jd):
 
     #直辖市/省(省级)
-    match=address[:2]
+    match=add[:2]
     #print("匹配",match)
-    for itemProvince in jsondata:
+    for itemProvince in jd:
         if re.search(match,itemProvince["name"]):
-            result.province = itemProvince["name"]
+            res.province = itemProvince["name"]
             cityList = itemProvince
-            #print(result.province)
-            address=cutdown(address,result.province)#混合地址去除省份
+            #print(res.province)
+            add=cutdown(add,res.province)#混合地址去除省份
 
             #判断第一级是否为直辖市,若是,添加回字符串(第二级用
             for direct in ["北京","上海","天津","重庆"]:
-                if direct == result.province:
-                    address = result.province + address
+                if direct == res.province:
+                    add = res.province + add
             
             break
-    if result.province == "":#目前台湾暂时没有数据
+    if res.province == "":#目前台湾暂时没有数据
         return 
     #-------------------------------------------------------------------------        
     #直辖市/市(地级)
     flagCity=0#默认没有查找到
-    match=address[:2]
+    match=add[:2]
     for itemCity in cityList["children"]:  
         if re.search(match,itemCity["name"]):
             flagCity=1
-            result.city = itemCity["name"]
-            #print(result.city)
+            res.city = itemCity["name"]
+            #print(res.city)
             
-            address = cutdown(address,result.city)
+            add = cutdown(add,res.city)
             areaList = itemCity
             break
 
     #-------------------------------------------------------------------------
     #区/县/县级市(县级)
-    match=address[:2]
+    match=add[:2]
     flagArea=0
     if(flagCity==1):
         for itemArea in areaList["children"]:
             if re.search(match,itemArea["name"]):
                 flagArea=1
-                result.area=itemArea["name"]
-                #print(result.area)
+                res.area=itemArea["name"]
+                #print(res.area)
             
-                address=cutdown(address,result.area)
+                add=cutdown(add,res.area)
                 townList=itemArea
                 break
     else:#处理市级缺失情况下的县级查找
         for itemCity in cityList["children"]:
             for itemArea in itemCity["children"]:
                 if re.search(match,itemArea["name"]):
-                    result.area=itemArea["name"]
-                    #print(result.area)
-                    address = cutdown(address,result.area)
+                    res.area=itemArea["name"]
+                    #print(res.area)
+                    add = cutdown(add,res.area)
                     townList=itemArea
                     flagArea=1
                     break
@@ -119,104 +108,103 @@ def segementFive(address):
 
     #---------------------------------------------------------------------
     #街道/镇/乡(乡镇级)
-    match=address[:2]
+    match=add[:2]
     if (flagArea==1):
         for itemTown in townList["children"]:
             if re.search(match,itemTown["name"]):
-                result.town = itemTown["name"]
-                #print(result.town)
-                address=cutdown(address,result.town)
+                res.town = itemTown["name"]
+                #print(res.town)
+                add=cutdown(add,res.town)
                 break
     else:#处理县级缺失情况下的乡镇级查找
         for itemArea in areaList["children"]:
             for itemTown in itemArea["children"]:
                 if re.search(match,itemTown['name']):
-                    result.town=itemTown["name"]
-                    #print(result.town)
-                    address=cutdown(address,result.town)
+                    res.town=itemTown["name"]
+                    #print(res.town)
+                    add=cutdown(add,res.town)
                     break
             else:
                 continue
             break
     #---------------------------------------------------------------------
-    result.street=address#在五级地址中使用street作为详细地址
+    res.street=add#在五级地址中使用street作为详细地址
     
-def segementSeven(detail):    
-    address=detail
+def segementSeven(detail,res):    
+    add=detail
     pattern=re.compile(r'(.*?[街])|(.*?[道])|(.*?[路])|(.*?[巷])|(.*?[大街])|(.*?[街道])')
     #pattern= re.compile(r'.+(路|街|巷|桥|道){1}')
-    match = pattern.search(address)
+    match = pattern.search(add)
     if(match):
-        result.street = match.group(0)
+        res.street = match.group(0)
     else:#可能存在缺失街道
-        result.street = ""
-    #print(result.street)
-    address = cutdown(address,result.street)
+        res.street = ""
+    #print(res.street)
+    add = cutdown(add,res.street)
     
     pattern=re.compile(r'(.*?[号])')
     #pattern = re.compile(r'.+(号){1}')
-    match = pattern.search(address)
+    match = pattern.search(add)
     if(match):
-        result.number = match.group(0)
+        res.number = match.group(0)
     else:#可能存在缺失门牌号
-        result.number = ""
-    #print(result.number)
-    address = cutdown(address,result.number)
-    result.building = address
-    #print(result.building)
+        res.number = ""
+    #print(res.number)
+    add = cutdown(add,res.number)
+    res.building = add
+    #print(res.building)
 
-def enhancement(address):
+def enhancement(add,res,jd):
     
     #直辖市/省(省级)
-    match=address[:2]
+    match=add[:2]
     #print("匹配",match)
     flagProvince=0#默认省级没有找到
-    for itemProvince in jsondata:
+    for itemProvince in jd:
         if re.search(match,itemProvince["name"]):
             flagProvince=1
-            result.province = itemProvince["name"]
+            res.province = itemProvince["name"]
             cityList = itemProvince
-            address=cutdown(address,result.province)#混合地址去除省份
+            add=cutdown(add,res.province)#混合地址去除省份
 
             #判断第一级是否为直辖市,若是,添加回字符串(第二级用
             for direct in ["北京","上海","天津","重庆"]:
-                if direct == result.province:
-                    address = result.province + address
-            
+                if direct == res.province:
+                    add = res.province + add
             break
     
     #-------------------------------------------------------------------------        
     #直辖市/市(地级)
     flagCity=0#默认没有查找到
-    match=address[:2]
+    match=add[:2]
     if(flagProvince==1):
         for itemCity in cityList["children"]:  
             if re.search(match,itemCity["name"]):
                 flagCity=1
-                result.city = itemCity["name"]
-                address = cutdown(address,result.city)
+                res.city = itemCity["name"]
+                add = cutdown(add,res.city)
                 areaList = itemCity
                 break
     else:#处理省级缺失情况下的市级查找
-        for itemProvince in jsondata:
+        for itemProvince in jd:
             for itemCity in itemProvince["children"]:
                 if re.search(match,itemCity["name"]):
                     flagCity=1
                     flagProvince=1
-                    result.province=itemProvince["name"]
-                    result.city=itemCity["name"]
-                    address=cutdown(address,result.city)
+                    res.province=itemProvince["name"]
+                    res.city=itemCity["name"]
+                    add=cutdown(add,res.city)
                     areaList=itemCity
     #-------------------------------------------------------------------------
     #区/县/县级市(县级)
-    match=address[:2]
+    match=add[:2]
     flagArea=0
     if(flagCity==1):
         for itemArea in areaList["children"]:
             if re.search(match,itemArea["name"]):
                 flagArea=1
-                result.area=itemArea["name"]
-                address=cutdown(address,result.area)
+                res.area=itemArea["name"]
+                add=cutdown(add,res.area)
                 townList=itemArea
                 break
     elif(flagCity==0 and flagProvince==1):#处理省级不缺失但是市级缺失情况下的县级查找
@@ -225,26 +213,26 @@ def enhancement(address):
                 if re.search(match,itemArea["name"]):
                     flagCity=1
                     flagArea=1
-                    result.city=itemCity["name"]
-                    result.area=itemArea["name"]
-                    address = cutdown(address,result.area)
+                    res.city=itemCity["name"]
+                    res.area=itemArea["name"]
+                    add = cutdown(add,res.area)
                     townList=itemArea
                     break
             else:
                 continue
             break
     else:#(flagCity==0 and flagProvince==0):#处理省级缺失并且市级缺失情况下的县级查找
-        for itemProvince in jsondata:
+        for itemProvince in jd:
             for itemCity in itemProvince["children"]:
                 for itemArea in itemCity["children"]:
                     if re.search(match,itemArea["name"]):
                         flagProvince=1
                         flagCity=1
                         flagArea=1
-                        result.province=itemProvince["name"]
-                        result.city=itemCity["name"]
-                        result.area=itemArea["name"]
-                        address=cutdown(address,result.area)
+                        res.province=itemProvince["name"]
+                        res.city=itemCity["name"]
+                        res.area=itemArea["name"]
+                        add=cutdown(add,res.area)
                         townList=itemArea
                         break
                 else:
@@ -254,23 +242,23 @@ def enhancement(address):
                 break
     #---------------------------------------------------------------------
     #街道/镇/乡(乡镇级)
-    match=address[:2]
+    match=add[:2]
     flagTown=0#默认没有查找到
     if (flagArea==1):
         for itemTown in townList["children"]:
             if re.search(match,itemTown["name"]):
-                result.town = itemTown["name"]
-                #print(result.town)
-                address=cutdown(address,result.town)
+                res.town = itemTown["name"]
+                #print(res.town)
+                add=cutdown(add,res.town)
                 break
     elif(flagArea==0 and flagCity==1):#处理市级不缺失但县级缺失情况下的乡镇级查找
         for itemArea in areaList["children"]:
             for itemTown in itemArea["children"]:
                 if re.search(match,itemTown['name']):
-                    result.area=itemArea["name"]
-                    result.town=itemTown["name"]
-                    #print(result.town)
-                    address=cutdown(address,result.town)
+                    res.area=itemArea["name"]
+                    res.town=itemTown["name"]
+                    #print(res.town)
+                    add=cutdown(add,res.town)
                     break
             else:
                 continue
@@ -283,10 +271,10 @@ def enhancement(address):
                         flagCity=1
                         flagArea=1
                         flagTown=1
-                        result.city=itemCity["name"]
-                        result.area=itemArea["name"]
-                        result.town=itemTown["name"]
-                        address=cutdown(address,result.town)
+                        res.city=itemCity["name"]
+                        res.area=itemArea["name"]
+                        res.town=itemTown["name"]
+                        add=cutdown(add,res.town)
                         break
                 else:
                     continue
@@ -296,74 +284,84 @@ def enhancement(address):
     #---------------------------------------------------------------------
     pattern=re.compile(r'(.*?[街])|(.*?[道])|(.*?[路])|(.*?[巷])|(.*?[大街])|(.*?[街道])')
     #pattern= re.compile(r'.+(路|街|巷|桥|道){1}')
-    match = pattern.search(address)
+    match = pattern.search(add)
     if(match):
-        result.street = match.group(0)
+        res.street = match.group(0)
     else:#可能存在缺失街道
-        result.street = ""
-    #print(result.street)
-    address = cutdown(address,result.street)
+        res.street = ""
+    #print(res.street)
+    add = cutdown(add,res.street)
     
     pattern=re.compile(r'(.*?[号])')
     #pattern = re.compile(r'.+(号){1}')
-    match = pattern.search(address)
+    match = pattern.search(add)
     if(match):
-        result.number = match.group(0)
+        res.number = match.group(0)
     else:#可能存在缺失门牌号
-        result.number = ""
-    #print(result.number)
-    address = cutdown(address,result.number)
-    result.building = address
-    #print(result.building)
-def formatFive():
-    data["姓名"]=result.name
-    data["手机"]=result.phone
-    data["地址"].append(result.province)
-    data["地址"].append(result.city)
-    data["地址"].append(result.area)
-    data["地址"].append(result.town)
-    data["地址"].append(result.street)
-    
+        res.number = ""
+    add = cutdown(add,res.number)
+    res.building = add
 
-def formatSeven():
-    formatFive()
-    data["地址"].append(result.number)
-    data["地址"].append(result.building)
-def solution(source):
-	
+def formatFive(res,dat):
+    dat["姓名"]=res.name
+    dat["手机"]=res.phone
+    dat["地址"].append(res.province)
+    dat["地址"].append(res.city)
+    dat["地址"].append(res.area)
+    dat["地址"].append(res.town)
+    dat["地址"].append(res.street)
+
+def formatSeven(res,dat):
+    dat["姓名"]=res.name
+    dat["手机"]=res.phone
+    dat["地址"].append(res.province)
+    dat["地址"].append(res.city)
+    dat["地址"].append(res.area)
+    dat["地址"].append(res.town)
+    dat["地址"].append(res.street)
+    dat["地址"].append(res.number)
+    dat["地址"].append(res.building)
+def solution(sourceStr,JD):#参数是原始字符串和js数据表
+    #用于输出的json
+    data={
+        "姓名":"",
+        "手机":"",
+        "地址":[]
+    }
+    result = Result()#实例化结论格式
     s = ""
-    s=source
+    s=sourceStr
     level = s.split("!")[0]#获取本条输入的难度
     level=eval(level)
-    #print(level)
 
     s=s.split("!")[1]#原始输入去掉"难度!"
     result.name = s.split(",")[0]#获取姓名
-    #print(result.name)
 
     s=s.split(",")[1]#原始输入剩余电话与地址
     phonenumber = getphone(s)#获取电话
-   
-    #print(phonenumber)
 
     result.phone = phonenumber
     address=getaddress(s,phonenumber)#address为初步的混合地址
-    #print(address)
     
     if level==1:
-        segementFive(address)
-        formatFive()
+        segementFive(address,result,JD)
+        formatFive(result,data)
     elif level==2:
-        segementFive(address)
-        segementSeven(result.street)
-        formatSeven()
+        segementFive(address,result,JD)
+        segementSeven(result.street,result)
+        formatSeven(result,data)
     else:
-        enhancement(address)
-        formatSeven()
+        enhancement(address,result,JD)
+        formatSeven(result,data)
     jsonresult=json.dumps(data,ensure_ascii=False)
     print(jsonresult)
     
 def main():
+    jsondata = {}
+    filepath = os.path.split(os.path.realpath(__file__))[0]
+    filepath = filepath + "\\" + "pcas-code.json"
+    with open(filepath,"r+",encoding='utf-8_sig')as f:
+        jsondata = json.load(f)#将已编码JSON字符串解码为Python字典
     while 1:
         try:
             inputraw=input();
@@ -371,7 +369,5 @@ def main():
                 break
         except EOFError:
             break
-        solution(inputraw)
-
+        solution(inputraw,jsondata)
 main()
-
